4a3981b9bd33f20e0a4dab350541266241796d21
Huilensolis <xywvanz@gmail.com>
2024-06-20 12:29:41 -0300

    chore: clean starter boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Build Square",
+  description:
+    "The hub for developers to build in public, share proejcts and contribute to open source",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="bg-background text-foreground">
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
