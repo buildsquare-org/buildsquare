0bedff3b2642df4e034e02bd50cb2e2feeca0764
Huilensolis <xywvanz@gmail.com>
2024-06-24 00:14:45 -0300

    feat: fix button props not extending button and make sign in page

---
+++ src/actions/auth/sign-in/index.ts
@@ -0,0 +1,18 @@
+const signIn = async (formData: FormData) => {
+  "use server";
+
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  return redirect("/protected");
+};
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,69 +1,60 @@
-import { headers } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+"use client";
+
 import { TextInput } from "@/components/ui/text-input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { TFormAreas } from "./sign-in.models";
 
 export default function SignInPage({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  const signIn = async (formData: FormData) => {
-    "use server";
+  const { formState, register, handleSubmit } = useForm<TFormAreas>({
+    mode: "onBlur",
+  });
 
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
+  const { errors, isValid, isSubmitting } = formState;
 
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/protected");
-  };
-
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
-  };
+  async function signUp(data: TFormAreas) {
+    return;
+  }
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <Label className="text-md" htmlFor="email">
-          Email
-        </Label>
-        <TextInput type="email" placeholder="youremail@gmail.com" />
-        <Label className="text-md" htmlFor="password">
-          Password
-        </Label>
-        <TextInput type="password" />
+    <div className="flex flex-col w-full h-full items-center justify-center gap-5">
+      <h1 className="dark:text-neutral-200 text-2xl font-semibold">
+        Welcome to Build Square!
+      </h1>
+      <form
+        onSubmit={handleSubmit(signUp)}
+        className="flex flex-col w-full justify-center gap-5 text-foreground"
+      >
+        <fieldset className="flex flex-col">
+          <Label className="text-md" htmlFor="email">
+            Email
+          </Label>
+          <TextInput
+            type="email"
+            id="email"
+            placeholder="youremail@gmail.com"
+          />
+        </fieldset>
+        <fieldset className="flex flex-col">
+          <Label className="text-md" htmlFor="password">
+            Password
+          </Label>
+          <TextInput type="password" id="password" placeholder="------" />
+        </fieldset>
+        <Button
+          className="py-5"
+          type="submit"
+          disabled={!isValid || isSubmitting}
+        >
+          Sign Up
+        </Button>
         {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+          <p className="mt-4 p-4 bg-neutral-700/10 dark:text-indigo-400 text-center">
             {searchParams.message}
           </p>
         )}
