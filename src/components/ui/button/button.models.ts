c0c0ff202d80d97c0a0ef88f7341531b7a893d38
Huilensolis <xywvanz@gmail.com>
2024-07-06 18:09:48 -0300

    feat: improve keyboard browsing on image input box

--- src/components/ui/button/button.models.ts
+++ src/components/ui/button/button.models.ts
@@ -9,7 +9,7 @@ export type RawProps = {
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const buttonVariants = cva(
-  "flex items-center justify-center gap-1 whitespace-nowrap rounded-sm text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "flex items-center justify-center gap-1 whitespace-nowrap rounded-sm text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-400 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
--- src/components/ui/image-input-box/image-input-box.component.tsx
+++ src/components/ui/image-input-box/image-input-box.component.tsx
@@ -1,92 +1,110 @@
 "use client";
 
-import { ChangeEvent, forwardRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import { TProps } from "./image-input-box.models";
 import { cn } from "@/utils/cn";
 import { ImagePlus, ImageUp, Trash } from "lucide-react";
 import { Button } from "../button";
 
-const ImageInputBox = forwardRef<HTMLInputElement, TProps>(
-  ({ onSelectImage, containerClassName, imageClassName }, ref) => {
-    const [selectedImageUrl, setSelectedImageUrl] = useState<string | null>(
-      null,
-    );
+function ImageInputBox({
+  onSelectImage,
+  containerClassName,
+  imageClassName,
+}: TProps) {
+  const [selectedImageUrl, setSelectedImageUrl] = useState<string | null>(null);
 
-    function handleSelectImage(e: ChangeEvent<HTMLInputElement>) {
-      if (!e.target.files) {
-        return;
-      }
-
-      const image = e.target.files[0];
-
-      if (!image) {
-        return;
-      }
-
-      setSelectedImageUrl(URL.createObjectURL(image));
-      onSelectImage(image);
+  function handleSelectImage(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) {
+      return;
     }
 
-    function handleUnselectImage() {
-      setSelectedImageUrl(null);
-      onSelectImage(null);
+    const image = e.target.files[0];
+
+    if (!image) {
+      return;
     }
 
-    return (
-      <div
-        className={cn(
-          `w-full ${selectedImageUrl ? "h-auto max-h-[536px]" : "h-44"} flex relative`,
-          containerClassName,
-        )}
-      >
-        <input
-          ref={ref}
-          type="file"
-          accept="image/*"
-          onChange={handleSelectImage}
-          className="w-full h-full absolute top-0 left-0 opacity-0"
-        />
-        {!selectedImageUrl && (
-          <div className="w-full h-full py-10 flex items-center justify-center border border-dashed border-neutral-700">
+    setSelectedImageUrl(URL.createObjectURL(image));
+    onSelectImage(image);
+  }
+
+  function handleUnselectImage() {
+    setSelectedImageUrl(null);
+    onSelectImage(null);
+  }
+
+  const imgInputRef = useRef<HTMLInputElement | null>(null);
+
+  return (
+    <div
+      className={cn(
+        `${selectedImageUrl ? "h-auto max-h-[536px]" : "h-44"} w-full rounded-sm flex relative border border-dashed border-neutral-700 focus-visible:border-indigo-400 focus-visible:outline-none has-[:focus-visible]:border-indigo-400`,
+        containerClassName,
+      )}
+      onFocus={() => {
+        if (!imgInputRef.current) return;
+        if (selectedImageUrl) return;
+
+        imgInputRef.current.focus();
+      }}
+      tabIndex={selectedImageUrl ? undefined : 0}
+    >
+      {!selectedImageUrl && (
+        <>
+          <input
+            ref={imgInputRef}
+            type="file"
+            accept="image/*"
+            onChange={handleSelectImage}
+            tabIndex={0}
+            className="w-full h-full absolute top-0 left-0 opacity-0 peer"
+          />
+          <div className="w-full h-full py-10 flex items-center justify-center">
             <div className="flex items-center gap-1 text-indigo-400">
               <ImagePlus className="w-4 h-4" />
               <span>Add Image</span>
             </div>
           </div>
-        )}
-        {selectedImageUrl && (
-          <>
-            <img
-              src={selectedImageUrl}
-              className={cn("w-full h-auto object-cover", imageClassName)}
-            />
-            <div className="flex flex-col gap-1 absolute top-1 right-1">
-              <Button variant="destructive" onClick={handleUnselectImage}>
-                <Trash className="w-4 h-4" /> Remove
-              </Button>
-              <Button
-                variant="secondary"
-                className="relative cursor-pointer"
-                asChild
-              >
-                <div>
-                  <input
-                    ref={ref}
-                    type="file"
-                    accept="image/*"
-                    onChange={handleSelectImage}
-                    className="w-full h-full absolute top-0 left-0 opacity-0"
-                  />
-                  <ImageUp className="w-4 h-4" /> Replace
-                </div>
-              </Button>
-            </div>
-          </>
-        )}
-      </div>
-    );
-  },
-);
+        </>
+      )}
+      {selectedImageUrl && (
+        <>
+          <img
+            src={selectedImageUrl}
+            className={cn("w-full h-auto object-cover", imageClassName)}
+          />
+          <div className="flex flex-col gap-1 absolute top-2 right-2">
+            <Button variant="destructive" onClick={handleUnselectImage}>
+              <Trash className="w-4 h-4" /> Remove
+            </Button>
+            <Button
+              variant="secondary"
+              className="relative cursor-pointer"
+              asChild
+              tabIndex={0}
+              onFocus={() => {
+                if (!imgInputRef.current) return;
+
+                imgInputRef.current.focus();
+              }}
+            >
+              <div>
+                <input
+                  ref={imgInputRef}
+                  type="file"
+                  accept="image/*"
+                  onChange={handleSelectImage}
+                  className="w-full h-full absolute top-0 left-0 opacity-0"
+                />
+                <ImageUp className="w-4 h-4" /> Replace
+              </div>
+            </Button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
 
 ImageInputBox.displayName = "ImageInputBox";
 
