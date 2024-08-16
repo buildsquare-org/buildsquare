3b81450d201ab48b0e1bd935b59768399235b507
Huilensolis <xywvanz@gmail.com>
2024-08-16 16:30:11 -0300

    fix: <a> being children of <a> and onclick firing when selecting text

--- src/app/components/project-card/project-card.component.tsx
+++ src/app/components/project-card/project-card.component.tsx
@@ -1,32 +1,43 @@
-import Link from "next/link";
+"use client";
+
 import { TProps } from "./project-card.models";
 import { ClientRouting } from "@/models/routing/client.routing";
 import { ProfileCardWrapper } from "../profile-card";
+import { useRouter } from "next/navigation";
 
 export function ProjectCard({ project }: TProps) {
+  const router = useRouter();
+
+  function redirectToProjectPage() {
+    if (window.getSelection() !== null) return; // if the user selected some text, dont continue
+
+    router.push(ClientRouting.projects().getById(project.title));
+  }
+
   return (
-    <Link href={ClientRouting.projects().getById(project.title)}>
-      <article className="dark:text-neutral-200 dark:bg-neutral-800 rounded-md p-2 flex flex-col gap-5">
-        {project.cover_image_url && (
-          <img
-            src={project.cover_image_url}
-            className="w-full aspect-video rounded-sm"
-          />
+    <article
+      onClick={redirectToProjectPage}
+      className="cursor-pointer dark:text-neutral-200 dark:bg-neutral-800 rounded-md p-2 flex flex-col gap-5"
+    >
+      {project.cover_image_url && (
+        <img
+          src={project.cover_image_url}
+          className="w-full aspect-video rounded-sm"
+        />
+      )}
+      <footer>
+        <div className="py-1">
+          <ProfileCardWrapper userId={project.owner_id} />
+        </div>
+        <h1 className="font-semibold text-lg">{project.title}</h1>
+        {project.description && (
+          <p className="text-neutral-400 text-sm">
+            {project.description.length > 300
+              ? project.description.slice(0, 300) + "..."
+              : project.description}
+          </p>
         )}
-        <footer>
-          <div className="py-1">
-            <ProfileCardWrapper userId={project.owner_id} />
-          </div>
-          <h1 className="font-semibold text-lg">{project.title}</h1>
-          {project.description && (
-            <p className="text-neutral-400 text-sm">
-              {project.description.length > 300
-                ? project.description.slice(0, 300) + "..."
-                : project.description}
-            </p>
-          )}
-        </footer>
-      </article>
-    </Link>
+      </footer>
+    </article>
   );
 }
